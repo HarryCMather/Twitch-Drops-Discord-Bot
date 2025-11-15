c05e41771fb363ea38d74450e50eaba23f757472
HarryCMather <harrycmather@gmail.com>
2025-11-15 23:42:34 +0000

    feat: Alter GCSettings & Log Startup Complete

--- src/TwitchDropsDiscordBot/Program.cs
+++ src/TwitchDropsDiscordBot/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Runtime;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TwitchDropsDiscordBot.Persistence;
 using TwitchDropsDiscordBot.Services;
@@ -9,6 +10,8 @@ internal static class Program
 {
     private static async Task Main()
     {
+        SetGcSettings();
+
         HostApplicationBuilder builder = Host.CreateApplicationBuilder();
 
         builder.Services.AddSingleton<SettingsFileRepository>()
@@ -26,7 +29,28 @@ internal static class Program
         builder.Services.AddHostedService<TwitchDropsCheckerBackgroundService>();
 
         IHost host = builder.Build();
-
+        LogStartupComplete(builder.Environment.IsDevelopment());
         await host.RunAsync();
+    }
+
+    private static void SetGcSettings()
+    {
+        const ulong gcHardLimitBytes = (ulong)200 * 1024 * 1024; // 200MB
+        AppContext.SetData("GCHeapHardLimit", gcHardLimitBytes);
+        GC.RefreshMemoryLimit();
+        GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+    }
+
+    private static void LogStartupComplete(bool isDevelopment)
+    {
+        string startupCompleteMessage = "Twitch Drops Discord Bot Started Successfully...\n" +
+                                        $"ServerGC: {GCSettings.IsServerGC}\n" +
+                                        $"LOHCompactionMode: {GCSettings.LargeObjectHeapCompactionMode}\n" +
+                                        $"IsDevelopment: {isDevelopment}\n" +
+                                        $"ProcessId: {Environment.ProcessId}\n" +
+                                        $"Hostname: {Environment.MachineName}";
+
+        Console.WriteLine(startupCompleteMessage);
+        // TODO: LOG THIS TO THE DISCORD TEXT CHANNEL!
     }
 }
