eadaa03a4000cf10b7fa0be730c6f92c3c2b3dc3
HarryCMather <harrycmather@gmail.com>
2025-11-15 00:11:10 +0000

    feat: Create settings model

---
+++ src/TwitchDropsDiscordBot/Models/Settings.cs
@@ -0,0 +1,25 @@
+﻿namespace TwitchDropsDiscordBot.Models;
+
+/// <summary>
+/// Model representing Settings.json
+/// </summary>
+public sealed class Settings
+{
+    /// <summary>
+    /// The ClientId, created on Twitch's Developer Portal and is unique per application.
+    /// </summary>
+    public string ClientId { get; set; }
+
+    /// <summary>
+    /// The ClientSecret, created on Twitch's Developer Portal and is unique per application.
+    /// This MUST not be exposed or committed.
+    /// </summary>
+    public string ClientSecret { get; set; }
+
+    /// <summary>
+    /// A list of game names to look for drops for.
+    /// The strings in this list should be/have been copied from the title at the top of the game's category page.
+    /// For example, https://www.twitch.tv/directory/category/tom-clancys-rainbow-six-siege has the exact title of "Tom Clancy's Rainbow Six Siege X".
+    /// </summary>
+    public IReadOnlyCollection<string> GameNames { get; set; }
+}
