133808fb67bcf40ef7208c3f31fa27eefe0c81dd
HarryCMather <harrycmather@gmail.com>
2025-11-16 00:43:11 +0000

    feat: Switch TwitchDropFinderService to use SunkwiApiClient implementation

--- src/TwitchDropsDiscordBot/Services/TwitchDropFinderService.cs
+++ src/TwitchDropsDiscordBot/Services/TwitchDropFinderService.cs
@@ -1,29 +1,28 @@
-﻿namespace TwitchDropsDiscordBot.Services;
+﻿using TwitchDropsDiscordBot.Models.SunkwiApi;
+using TwitchDropsDiscordBot.Persistence;
+
+namespace TwitchDropsDiscordBot.Services;
 
 public sealed class TwitchDropFinderService
 {
-    private readonly TwitchGameIdFinderService _twitchGameIdFinderService;
-    private readonly TwitchStreamsFinderService _twitchStreamsFinderService;
+    private readonly SunkwiApiClient _sunkwiApiClient;
 
-    public TwitchDropFinderService(TwitchGameIdFinderService twitchGameIdFinderService, TwitchStreamsFinderService twitchStreamsFinderService)
+    public TwitchDropFinderService(SunkwiApiClient sunkwiApiClient)
     {
-        _twitchGameIdFinderService = twitchGameIdFinderService;
-        _twitchStreamsFinderService = twitchStreamsFinderService;
+        _sunkwiApiClient = sunkwiApiClient;
     }
 
-    public async Task FindNewDropsAsync(string clientId, string clientSecret, IReadOnlyCollection<string> gameNames)
+    public async Task FindNewDropsAsync(IReadOnlyCollection<string> gameNames)
     {
-        const string dropsEnabledTag = "DropsEnabled";
-
         try
         {
-            Dictionary<string, uint> gameIdMap = await _twitchGameIdFinderService.GetGameIdMapAsync(clientId, clientSecret, gameNames);
+            Console.WriteLine("Checking for new Twitch drops...");
+            IAsyncEnumerable<GetDropsResponse> getDropsResponse = _sunkwiApiClient.GetDropsAsync();
+            List<GetDropsResponse> dropsForRequestedGames = await ExtractDropsForRequestedGames(getDropsResponse, gameNames);
 
-            foreach (KeyValuePair<string, uint> gameMapKvp in gameIdMap)
+            foreach (GetDropsResponse drop in dropsForRequestedGames)
             {
-                Console.WriteLine($"Trying to find twitch drops for game '{gameMapKvp.Key}'");
-                bool foundStreamsWithTag = await _twitchStreamsFinderService.AnyStreamsWithTagAsync(clientId, clientSecret, gameMapKvp.Value, dropsEnabledTag);
-                Console.WriteLine($"Game '{gameMapKvp.Key}' has drops enabled: {foundStreamsWithTag}");
+                Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(drop));
             }
         }
         catch (HttpRequestException exception)
@@ -33,6 +32,52 @@ public sealed class TwitchDropFinderService
         catch (Exception exception)
         {
             Console.WriteLine($"Exception: {exception.Message}");
+        }
+    }
+
+    private static async Task<List<GetDropsResponse>> ExtractDropsForRequestedGames(IAsyncEnumerable<GetDropsResponse> drops, IReadOnlyCollection<string> requestedGameNames)
+    {
+        HashSet<string> requestedGameNamesSet = new(requestedGameNames);
+
+        List<GetDropsResponse> dropsForRequestedGames = [];
+        await foreach (GetDropsResponse drop in drops)
+        {
+            if (requestedGameNamesSet.Contains(drop.GameDisplayName))
+            {
+                Console.WriteLine($"Found drop for game '{drop.GameDisplayName}'");
+
+                RemoveInactiveRewards(drop.Rewards);
+                RemoveRewardsWithNoTimeBasedDrops(drop.Rewards);
+
+                if (drop.Rewards.Count > 0)
+                {
+                    dropsForRequestedGames.Add(drop);
+                }
+                else
+                {
+                    Console.WriteLine($"After removing inactive rewards and rewards without time-based drops, there were no rewards left for game '{drop.GameDisplayName}'. Therefore, this drop won't be used.");
+                }
+            }
+        }
+
+        return dropsForRequestedGames;
+    }
+
+    private static void RemoveInactiveRewards(List<GetDropsReward> rewards)
+    {
+        int removedRewards = rewards.RemoveAll(reward => reward.Status != "ACTIVE");
+        if (removedRewards > 0)
+        {
+            Console.WriteLine($"{removedRewards} rewards were removed from the drop as they were not active.");
+        }
+    }
+
+    private static void RemoveRewardsWithNoTimeBasedDrops(List<GetDropsReward> rewards)
+    {
+        int removedRewards = rewards.RemoveAll(reward => reward.TimeBasedDrops.Count == 0);
+        if (removedRewards > 0)
+        {
+            Console.WriteLine($"{removedRewards} rewards were removed from the drop as they did not contain any time-based drops.");
         }
     }
 }
