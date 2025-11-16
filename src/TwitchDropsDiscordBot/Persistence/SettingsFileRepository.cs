7a9d5404e5ba046904dc9a0ff4c231cc62144d1d
HarryCMather <harrycmather@gmail.com>
2025-11-16 02:15:21 +0000

    chore: XML comments for SettingsFileRepository

--- src/TwitchDropsDiscordBot/Persistence/SettingsFileRepository.cs
+++ src/TwitchDropsDiscordBot/Persistence/SettingsFileRepository.cs
@@ -3,8 +3,16 @@ using TwitchDropsDiscordBot.Models;
 
 namespace TwitchDropsDiscordBot.Persistence;
 
+/// <summary>
+/// Implementation of Settings File Repository.
+/// Performs file-related actions for retrieving Settings.
+/// </summary>
 public sealed class SettingsFileRepository
 {
+    /// <summary>
+    /// Reads and Deserializes Settings from Settings.json.
+    /// </summary>
+    /// <returns></returns>
     public async Task<Settings> GetSettingsFromFileAsync()
     {
         const string filePath = "Settings.json";
