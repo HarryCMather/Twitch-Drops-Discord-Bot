9420f9e46766f24658ecfdd2c0238e129c516e3d
HarryCMather <harrycmather@gmail.com>
2025-11-16 16:09:08 +0000

    feat: Add Discord Settings & Discord.NET

--- src/TwitchDropsDiscordBot/Models/Settings.cs
+++ src/TwitchDropsDiscordBot/Models/Settings.cs
@@ -6,6 +6,17 @@
 public sealed class Settings
 {
     /// <summary>
+    /// The Discord Bot Token, which is required to send messages to your server's text channel.
+    /// This can be generated here:  https://discord.com/developers/applications
+    /// </summary>
+    public string DiscordBotToken { get; set; }
+
+    /// <summary>
+    /// The Discord ChannelID of the text channel where you want the notifications to be sent.
+    /// </summary>
+    public string DiscordChannelId { get; set; }
+
+    /// <summary>
     /// A list of game names to look for drops for.
     /// The strings in this list should be/have been copied from the title at the top of the game's category page.
     /// For example, https://www.twitch.tv/directory/category/tom-clancys-rainbow-six-siege has the exact title of "Tom Clancy's Rainbow Six Siege X".
