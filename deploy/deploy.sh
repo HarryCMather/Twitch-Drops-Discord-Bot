fd7df00753fb780b4dcb52d4f7fe4964ee8e1323
HarryCMather <harrycmather@gmail.com>
2026-04-15 01:01:02 +0100

    feat: Target appsettings.json instead of Settings.json & Remove AlertHistory.txt

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,8 +5,8 @@
 #     /home/user/TwitchDropsDiscordBot
 #        TwitchDropsDiscordBot
 #        Deploy
-#        Settings.json
-# 2 - The Settings.json contains all substituted variables, so will overwrite the extracted artifact's Settings.json file.
+#        appsettings.json
+# 2 - The appsettings.json contains all substituted variables, so will overwrite the extracted artifact's Settings.json file.
 # 3 - 7-zip is installed, though this can be switched to other extraction tools based on preferences...
 # 4 - The twitchdropsdiscordbot service has already been set-up as per the instructions in /deploy/twitchdropsdiscordbot.service in this repository.
 # 5 - When this script is executed, the built artifact (7z files) from the build pipeline should have already been copied to "~/TwitchDropsDiscordBot/Deploy".
@@ -19,14 +19,8 @@ echo "Extracting the Bot and removing the archive...";
 7z x ./TwitchDropsDiscordBot.zip;
 rm ./TwitchDropsDiscordBot.zip;
 
-echo "Backing up the previous Alert History file...";
-cp -f ../TwitchDropsDiscordBot/AlertHistory.txt ../AlertHistory.txt;
-
-echo "Copying the Alert History file into deploy folder";
-cp -f ../AlertHistory.txt ./AlertHistory.txt;
-
-echo "Overwriting the Settings file with local copy...";
-cp -f ../Settings.json ./Settings.json;
+echo "Overwriting the appsettings file with local copy...";
+cp -f ../appsettings.json ./appsettings.json;
 
 echo "Stopping the Bot...";
 sudo service twitchdropsdiscordbot stop;
