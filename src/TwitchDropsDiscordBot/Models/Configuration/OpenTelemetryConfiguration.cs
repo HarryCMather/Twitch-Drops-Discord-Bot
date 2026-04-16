c6d2c577ef6e5b5e26ec198071bcef299576d87a
HarryCMather <94319888+HarryCMather@users.noreply.github.com>
2026-04-16 17:40:01 +0100

    feat: Add OpenTelemetry support

---
+++ src/TwitchDropsDiscordBot/Models/Configuration/OpenTelemetryConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchDropsDiscordBot.Models.Configuration;
+
+public sealed record OpenTelemetryConfiguration
+{
+    public const string SectionKey = "OpenTelemetry";
+
+    /// <summary>
+    /// Whether the application should log any recorded Logs, Metrics or Traces to the relevant exporter.
+    /// </summary>
+    public bool Enabled { get; init; }
+
+    /// <summary>
+    /// The OpenTelemetry exporter endpoint url to publish Logs, Metrics or Traces to.
+    /// </summary>
+    public string ExporterEndpoint { get; init; }
+}
